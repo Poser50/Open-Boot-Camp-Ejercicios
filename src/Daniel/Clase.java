0c25205c26cbc874ac687c280d54383fca98d2e9
Poser50 <120696549+Poser50@users.noreply.github.com>
2022-12-15 21:51:25 -0500

    Ejercicios Funciones

---
+++ src/Daniel/Clase.java
@@ -0,0 +1,17 @@
+package Daniel;
+
+public class Clase {
+    public static void main(String[]args){
+        Coche miCoche = new Coche();
+        miCoche.AnadirPuerta();
+        System.out.println(miCoche.Puertas);
+    }
+}
+
+class Coche{
+    public int Puertas=4;
+
+    public void AnadirPuerta(){
+        this.Puertas++;
+    }
+}
---
+++ src/Daniel/Suma.java
@@ -0,0 +1,12 @@
+package Daniel;
+
+public class Suma {
+    public static void main(String[] args){
+        int resultado = suma(25, 50, 75);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c){
+        return a+b+c;
+    }
+}
